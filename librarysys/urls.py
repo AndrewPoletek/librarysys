8c9eee06d9ae6ceaf09bfe6456e98c9a6109a449
Andrzej Poletek <andrzej>
2021-01-10 13:00:56 +0100

    im repair logout button

--- librarysys/urls.py
+++ librarysys/urls.py
@@ -20,5 +20,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login')
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
